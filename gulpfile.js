f8e427565f32dd0908f71282ea91b37b844e2c58
wenyizhu-vera <128750029+wenyizhu-vera@users.noreply.github.com>
2023-04-11 07:57:48 -0500

    041123

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ const replace = require('gulp-replace');
 
 gulp.task('update-href', function () {
     return gulp.src('**/*.html')
-        .pipe(replace(/sp_sandbox.html"/g, 'href="sp_sandbox.html"'))
-        // .pipe(replace(/href="map_api.html"/g, 'href="sb_mapapi.html"'))
+        .pipe(replace(/href="sp_basic info.html"/g, 'href="sp_basicinfo.html"'))
         .pipe(gulp.dest('./', { overwrite: true }));
 });
 
